a13ec1bf82ad21fbda07b2b8c1471bc4d089623d
hugo rath <hugo.rath@epitech.eu>
2023-05-29 17:36:00 +0200

    [add] my_malloc

--- sources/main.c
+++ sources/main.c
@@ -8,10 +8,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+
+typedef struct metadata {
+    size_t size;
+    int is_free;
+    struct metadata* next;
+} metadata_t;
+
+metadata_t* head = NULL;
+
+void *my_malloc(size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
+    size_t total_size = sizeof(metadata_t) + size;
+    metadata_t *current_block = head;
+    while (current_block != NULL) {
+        if (current_block->is_free && current_block->size >= size) {
+            current_block->is_free = 0;
+            return (void *)(current_block + 1);
+        }
+        current_block = current_block->next;
+    }
+    void *block = sbrk(total_size);
+    if (block == (void *) -1) {
+        return NULL;
+    }
+    metadata_t *header = block;
+    header->size = size;
+    header->is_free = 0;
+    header->next = head;
+    head = header;
+    return (void *)(header + 1);
+}
 
 int main(int argc, char *argv[])
 {
-    char *str = malloc(sizeof(char) * (strlen("Hello World") - 1));
-    free(str);
+    char *str = malloc((strlen("Hello World") - 1));
+    char *str_part_two = my_malloc((strlen("hello world") - 1));
     return 0;
 }
