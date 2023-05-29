815c53bec29ba1f4795a79b92eae61599c527591
hugo rath <hugo.rath@epitech.eu>
2023-05-29 16:22:56 +0200

    exo 1

--- sources/main.c
+++ sources/main.c
@@ -5,7 +5,13 @@
 ** memory
 */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
+    char *str = malloc(sizeof(char) * (strlen("Hello World") - 1));
+    free(str);
     return 0;
 }
